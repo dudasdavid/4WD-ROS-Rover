8d8f2ccf8e2be849abc8b211dab6e99593e25381
david.dudas <david.dudas@outlook.com>
2020-04-08 12:46:06 +0200

    EXTI config added

--- STM32F103/Core/Src/main.c
+++ STM32F103/Core/Src/main.c
@@ -572,6 +572,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
 }
 
 /* USER CODE BEGIN 4 */
